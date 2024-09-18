5d55fe7419451f4935c03ac2613bc886f2251d9e
ming <zzzzz124987@gmail.com>
2024-09-18 12:42:03 +0800

    777

---
+++ _nuxt/6UoyeMfO.js
@@ -0,0 +1,1 @@
+import{X as i,r}from"./CCoAYo87.js";const y=i("verifyStore",()=>{const t=r([]),s=r({});return{verifyLists:t,setVerifyLists:e=>{t.value=e},verifyDetail:s,setVerifyDetail:e=>{s.value=e}}});export{y as u};
